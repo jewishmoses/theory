576300ab56a1f670662853ad8f83b8697139c432
jewishmoses <work@xmoses.com>
2021-09-01 21:33:32 +0300

    fixed bug

--- questions/static/app.js
+++ questions/static/app.js
@@ -4,11 +4,15 @@ buttons.forEach(button => {
 
     button.addEventListener('click', event => {
 
+        const btns = document.querySelectorAll('button.answer');
+        btns.forEach(btn => { btn.disabled = true; } );
+
         if (button.id == 'correct') {
 
             button.classList.add('right');
             setTimeout(function () { location.reload(); }, 2000);
             return;
+
         }
 
         button.classList.add('wrong');
